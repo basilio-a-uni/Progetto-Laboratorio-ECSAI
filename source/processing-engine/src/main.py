6789198b2d0b545bee1f24d59a36241f458f54e6
basilio-a-uni <alessandrino.2046353@studenti.uniroma1.it>
2026-03-07 09:48:43 +0100

    Removed trailing file

--- source/processing-engine/src/main.py
+++ source/processing-engine/src/main.py
@@ -3,7 +3,27 @@ import json
 import os
 import time
 
+from collections import defaultdict
+
 import database
+import sqlite3
+
+class State():
+    def __init__(self, sensor_data = {}, current_rules = defaultdict(list)):
+        self.sensor_data = sensor_data
+        self.current_rules = current_rules
+
+    def load_rules(self):
+        conn = sqlite3.connect(os.getenv("DATABASE_URL"))
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM rules")
+        rows = cur.fetchall()
+
+        for row in rows:
+            current_rules[row]
+            
+
 
 def get_connection():
     rabbit_host = os.getenv('RABBITMQ_HOST', 'localhost')
@@ -42,6 +62,9 @@ def start_consuming():
     )
     channel.start_consuming()
 
+
+
 if __name__ == "__main__":
+    state = State()
     database.init_db()
     start_consuming()
